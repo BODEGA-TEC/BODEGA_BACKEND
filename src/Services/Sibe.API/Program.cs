f0965230f3793b9b952bdac8be21b869a9892f95
Shakime Sparks <jeykimers.johnson@gmail.com>
2023-11-22 06:41:47 -0600

    upd: remove swagger

--- src/Services/Sibe.API/Program.cs
+++ src/Services/Sibe.API/Program.cs
@@ -83,7 +83,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();

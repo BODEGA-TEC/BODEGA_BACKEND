78200e286149aedba32eb2ce9ef5aa87b09abfe2
Shakime Sparks <jeykimers.johnson@gmail.com>
2023-12-14 13:27:18 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -1,27 +1,30 @@
-from ldap3 import Server, Connection, ALL, SUBTREE, ALL_ATTRIBUTES
+from ldap3 import Server, Connection, ALL, SUBTREE
 
 # Define el servidor y la conexión
-ldap_server = 'estudiantes.ie.tec.ac.cr'
-ldap_port = 389
-base_dn = 'dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr'
-username = 'cn=sibe,dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr'
-password = 'Cg7X4k57QWSc'
+s = Server('estudiantes.ie.tec.ac.cr', get_info=ALL)
+c = Connection(s, user='cn=sibe,dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', password='Cg7X4k57QWSc')
 
-# Construir el servidor
-server = Server(ldap_server, port=ldap_port, get_info=ALL)
+# Intenta realizar la conexión manualmente
+try:
+    c.bind()
+    print("Conexión exitosa.")
 
-# Intenta conectar y realizar la búsqueda
-with Connection(server, user=username, password=password, auto_bind=True) as conn:
-    # Realizar una búsqueda
-    conn.search(search_base=base_dn, search_filter='(objectClass=*)', search_scope=SUBTREE, attributes=ALL_ATTRIBUTES)
+    # Define la base de búsqueda y el filtro de búsqueda
+    base_dn = 'dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr'
+    search_filter = '(objectClass=*)'
 
-    # Imprimir los resultados en una tabla
-    print("{:<20} {:<20}".format("Nombre", "Valor"))
-    print("=" * 40)
-    for entry in conn.entries:
-        for attribute in entry.entry_attributes:
-            print("{:<20} {:<20}".format(attribute, entry[attribute]))
-        print("-" * 40)
+    # Realiza la búsqueda
+    if c.search(base_dn, search_filter, SUBTREE):
+        print("Búsqueda exitosa.")
+        for entry in c.response:
+            print(entry)
+    else:
+        print("Error en la búsqueda.")
+except Exception as e:
+    print(f"Error de autenticación: {e}")
+finally:
+    # Cerrar la conexión
+    c.unbind()
 
 
 # from ldap3 import Server, Connection, ALL

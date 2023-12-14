a111c0fec260c0ca18be53ec77618db6b02a2621
Shakime Sparks <jeykimers.johnson@gmail.com>
2023-12-14 13:11:34 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -1,20 +1,9 @@
-import ldap
+from ldap3 import Server, Connection, ALL
 
-# Inicializa la conexión LDAP
-l = ldap.initialize('ldap://estudiantes.ie.tec.ac.cr')
+# Define el servidor y la conexión
+s = Server('estudiantes.ie.tec.ac.cr', get_info=ALL)
+c = Connection(s, user='cn=sibe,dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', password='Cg7X4k57QWSc', auto_bind=True)
 
-# Define el nombre de usuario y la contraseña
-username = "uid=sibe,ou=People,dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr"
-password = "Cg7X4k57QWSc"
-
-try:
-    # Establece la versión del protocolo LDAP
-    l.protocol_version = ldap.VERSION3
-
-    # Intenta iniciar sesión en el servidor LDAP
-    l.simple_bind_s(username, password)
+# Imprime un mensaje si la conexión es exitosa
+if c.bound:
     print("Conexión exitosa.")
-except ldap.INVALID_CREDENTIALS:
-    print("Tu nombre de usuario o contraseña es incorrecto.")
-except ldap.LDAPError as e:
-    print(e)

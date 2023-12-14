dd040ffe4b2b5c0d737b08ad14509278b559fb23
Shakime Sparks <jeykimers.johnson@gmail.com>
2023-12-14 13:41:14 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -1,8 +1,12 @@
 from ldap3 import Server, Connection, ALL, SUBTREE
 
+domain = 'estudiantes.ie.tec.ac.cr'
+base_dn = 'dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr'
+username = 'cn=sibe,'+base_dn
+
 # Define el servidor y la conexión
-s = Server('estudiantes.ie.tec.ac.cr', get_info=ALL)
-c = Connection(s, user='cn=sibe,dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', password='Cg7X4k57QWSc')
+s = Server(domain, get_info=ALL)
+c = Connection(s, user=username, password='Cg7X4k57QWSc')
 
 # Intenta realizar la conexión manualmente
 try:
@@ -10,16 +14,18 @@ try:
     print("Conexión exitosa.")
 
     # Define la base de búsqueda y el filtro de búsqueda
-    base_dn = 'dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr'
-    search_filter = '(objectClass=*)'
+    search_filter = '(objectClass=organizationalUnit)'
 
     # Realiza la búsqueda
     if c.search(base_dn, search_filter, SUBTREE):
-        print("Búsqueda exitosa.")
+        print("Unidades Organizativas encontradas:")
         for entry in c.response:
-            print(entry)
+            print(entry['dn'])
+            # for attribute, values in entry['attributes'].items():
+            #     print(f"  {attribute}: {values}")
     else:
-        print("Error en la búsqueda.")
+        print("No se encontraron unidades organizativas.")
+                
 except Exception as e:
     print(f"Error de autenticación: {e}")
 finally:

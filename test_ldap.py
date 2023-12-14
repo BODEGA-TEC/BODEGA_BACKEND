330c2d2500fc8378c72e426cfadaf7d1c04fcf2c
Shakime Sparks <jeykimers.johnson@gmail.com>
2023-12-14 13:19:10 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -1,9 +1,26 @@
+# from ldap3 import Server, Connection, ALL
+
+# # Define el servidor y la conexión
+# s = Server('estudiantes.ie.tec.ac.cr', get_info=ALL)
+# c = Connection(s, user='cn=sibe,dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', password='Cg7X4k57QWSc', auto_bind=True)
+
+# # Imprime un mensaje si la conexión es exitosa
+# if c.bound:
+#     print("Conexión exitosa.")
+
+
 from ldap3 import Server, Connection, ALL
 
 # Define el servidor y la conexión
 s = Server('estudiantes.ie.tec.ac.cr', get_info=ALL)
-c = Connection(s, user='cn=sibe,dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', password='Cg7X4k57QWSc', auto_bind=True)
+c = Connection(s, user='cn=sibe,dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', password='Cg7X4k57QWSc')
 
-# Imprime un mensaje si la conexión es exitosa
-if c.bound:
+# Intenta realizar la conexión manualmente
+try:
+    c.bind()
     print("Conexión exitosa.")
+except Exception as e:
+    print(f"Error de autenticación: {e}")
+finally:
+    # Cerrar la conexión
+    c.unbind()

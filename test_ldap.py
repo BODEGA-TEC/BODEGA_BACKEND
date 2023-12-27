9c24adecca2c4037b5e722f0fd21ece639aa0d23
Shakime Sparks <s.jeykime@gmail.com>
2023-12-27 17:49:04 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -29,7 +29,7 @@ def print_entities_in_ou():
 
         try:
             
-            if 'C' in e['name'].value[0]:
+            if 'cacerdas' in e['sAMAccountName'].value:
                 # print("=" * 80)
                 names.append(e['name'].value)
                 # print(e)

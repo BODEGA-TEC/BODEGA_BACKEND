06d91123856ace21233e61b78d3d9fe13d13aa3b
Shakime Sparks <s.jeykime@gmail.com>
2023-12-27 14:08:02 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -7,16 +7,11 @@ user_name = 'sibe'
 password = 'Cg7X4k57QWSc'
 
 format_string = '{:25} {:>6} {:19} {:19} {}'
-print(format_string.format('User', 'Logins', 'Last Login', 'Expires', 'Description'))
+print(format_string.format('User', 'DisplayName', 'userPrincipalName', 'Expires'))
 
 server = Server(server_name, get_info=ALL)
 conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password=password, authentication=NTLM, auto_bind=True)
 conn.search('dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', '(objectclass=*)', attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
 
-for e in conn.entries:
-    if 'description' in e:
-        desc = e.description.value
-    else:
-        print(conn.entries)
-
-    print(format_string.format(str(e.name), str(e.logonCount.value), str(e.lastLogon.value)[:19], str(e.accountExpires.value)[:19], desc))
+for e in islice(conn.entries, 3):
+    print(format_string.format(str(e.name), str(e.displayName), str(e.userPrincipalName)[:19], str(e.accountExpires.value)[:19]))

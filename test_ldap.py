91b224563121c069be071527e7984c42501b5bc9
Shakime Sparks <s.jeykime@gmail.com>
2023-12-27 13:12:55 -0600

    ldap testing nuevamente

--- test_ldap.py
+++ test_ldap.py
@@ -1,18 +1,23 @@
-from ldap3 import Server, Connection, exceptions
+import sys
+from ldap3 import Server, Connection, ALL, NTLM, ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES, AUTO_BIND_NO_TLS, SUBTREE
+from ldap3.core.exceptions import LDAPCursorError
 
-def verify_ldap_domain(domain):
+server_name = 'ie-estudiantes'
+domain_name = 'estudiantes.ie.tec.ac.cr'
+user_name = 'sibe'
+password = 'Cg7X4k57QWSc'
+
+format_string = '{:25} {:>6} {:19} {:19} {}'
+print(format_string.format('User', 'Logins', 'Last Login', 'Expires', 'Description'))
+
+server = Server(server_name, get_info=ALL)
+conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password=password, authentication=NTLM, auto_bind=True)
+conn.search('dc={},dc=local'.format(domain_name), '(objectclass=*)', attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+
+for e in conn.entries:
     try:
-        # Intenta conectar al servidor LDAP sin proporcionar credenciales
-        server = Server(domain)
-        connection = Connection(server, auto_bind=True)
-        connection.unbind()
-        return True  # Si no hay excepción, el dominio existe
-    except exceptions.LDAPException:
-        return False  # Si hay excepción, el dominio no existe o no es accesible
+        desc = e.description.value if 'description' in e else ""
+    except LDAPCursorError:
+        desc = "Error"
 
-# Uso del método
-domain_to_check = "estudiantes.ie.tec.ac.cr"
-if verify_ldap_domain(domain_to_check):
-    print(f"El dominio LDAP '{domain_to_check}' es válido.")
-else:
-    print(f"El dominio LDAP '{domain_to_check}' no es válido o no es accesible.")
+    print(format_string.format(str(e.name), str(e.logonCount.value), str(e.lastLogon.value)[:19], str(e.accountExpires.value)[:19], desc))

381983f261f4d4325ccd376445900382f55f7114
Shakime Sparks <s.jeykime@gmail.com>
2023-12-27 14:29:02 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -12,15 +12,41 @@ format_string = '{:25} {:>6} {:19} {:19}'
 server = Server(server_name, get_info=ALL)
 conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password=password, authentication=NTLM, auto_bind=True)
 
-filter = '(objectClass=organizationalUnit)'
-conn.search('dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', filter, attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+def print_entities_in_ou(ou_name):
+    ou_filter = f'(&(objectClass=*)(ou={ou_name}))'
+    conn.search('dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', ou_filter, attributes=[ALL])
 
-for index, e in enumerate(conn.entries):
-        # if index >= 4:
-        #     break  # Sale del bucle después de imprimir las primeras 3 entradas    
-    ou_name = e.ou.value
-    print(f"- {ou_name}")
-    # print(f"Entry: {e.dn}")
+    print(f"Listado de entidades en la OU - '{ou_name}':")
+    for index, e in enumerate(conn.entries):
+        if index >= 3:
+            break  # Sale del bucle después de imprimir las primeras 3 entradas
+
+        print(f"- {e}")
+
+# Llamada a la función para imprimir las entidades en la OU "Docentes"
+print_entities_in_ou('Docentes')
+
+
+
+
+
+
+
+
+
+# def test():
+#     filter = '(objectClass=organizationalUnit)'
+#     conn.search('dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', filter, attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+
+#     for index, e in enumerate(conn.entries):
+#             # if index >= 4:
+#             #     break  # Sale del bucle después de imprimir las primeras 3 entradas    
+#         ou_name = e.ou.value
+#         print(f"- {ou_name}")
+        
+# test()
+
+#     # print(f"Entry: {e.dn}")
     # for attribute in e:
     #     print(f"  {attribute.key}: {attribute.values}")
     # print("=" * 50)

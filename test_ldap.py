f225a8ab7e7787f06ef7d909500583c198c7fd8f
Shakime Sparks <s.jeykime@gmail.com>
2023-12-27 17:47:20 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -29,7 +29,7 @@ def print_entities_in_ou():
 
         try:
             
-            if 'M' in e['name'].value[0]:
+            if 'C' in e['name'].value[0]:
                 # print("=" * 80)
                 names.append(e['name'].value)
                 # print(e)

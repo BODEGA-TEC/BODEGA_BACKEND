722f43e61c30901b930f1b7b8cf18cc3cff302b6
Shakime Sparks <s.jeykime@gmail.com>
2023-12-27 15:02:38 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -14,7 +14,7 @@ conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password
 
 def print_entities_in_ou():
     
-    ou_filter = '(&(objectCategory=person)(objectClass=user)(OU=Docentes))'
+    ou_filter = '(&(objectCategory=person)(objectClass=user))'
     conn.search('dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', ou_filter, attributes=[ALL_ATTRIBUTES])
 
     for index, e in enumerate(conn.entries):

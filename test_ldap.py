9bcfb26a9857455a2f521d17407427e822d57a82
Shakime Sparks <s.jeykime@gmail.com>
2023-12-27 16:16:34 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -14,7 +14,7 @@ conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password
 
 def print_entities_in_ou():
     
-    ou_filter = '(&(objectClass=*))'
+    ou_filter = '(objectClass=organizationalUnit)'
     conn.search('dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', ou_filter, attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
 
     i = 0
@@ -28,6 +28,7 @@ def print_entities_in_ou():
         
         print(e)
         print("=" * 80)
+        print()
 
         i+=1
         if i>=10:

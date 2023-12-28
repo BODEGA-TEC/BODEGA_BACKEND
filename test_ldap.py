d5978e670dddda69cf836d29ce7da428847a17dc
Shakime Sparks <s.jeykime@gmail.com>
2023-12-27 18:11:28 -0600

    Update test_ldap.py

--- test_ldap.py
+++ test_ldap.py
@@ -14,59 +14,53 @@ conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password
 #(memberOf=CN=allestudiantes,OU=Grupos,DC=estudiantes,DC=ie,DC=tec,DC=ac,DC=cr)
 
 
-base_dn = 'dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr'  # Ajusta según tu estructura LDAP      
+base_dn = 'ou=Estudiantes,dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr'  # Ajusta según tu estructura LDAP      
 filter = '(&(objectClass=person))'
 
-conn.search(search_base=base_dn, search_filter=filter, attributes=[ALL_ATTRIBUTES], search_scope=SUBTREE)
+# conn.search(search_base=base_dn, search_filter=filter, attributes=[ALL_ATTRIBUTES], search_scope=SUBTREE)
 
-i = 0
-print("\n" * 2)
-names = []
-for e in conn.entries:
+# i = 0
+# print("\n" * 2)
+# names = []
+# for e in conn.entries:
 
-    # i+=1
-    # if i>=4:
-    #     break
+#     # i+=1
+#     # if i>=4:
+#     #     break
 
-    try:
-        # if 'M' in e['name'].value[0]:
-        #     # print("=" * 80)
-        #     names.append(e['name'].value)
-        print(e)
+#     try:
+#         # if 'M' in e['name'].value[0]:
+#         #     # print("=" * 80)
+#         #     names.append(e['name'].value)
+#         print(e)
 
-        #     print()
-        # print(e['sAMAccountName'])
-        # print(e)
+#         #     print()
+#         # print(e['sAMAccountName'])
+#         # print(e)
 
-    except:
-        continue
-
-
-print("\n" * 4)
-
-names.sort()
-for n in names:
-    print(n)
+#     except:
+#         continue
 
 
+# print("\n" * 4)
+
+# names.sort()
+# for n in names:
+#     print(n)
 
 
 
 
-# def test():
-#     filter = '(objectClass=organizationalUnit)'
-#     conn.search('dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', filter, attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
 
-#     for index, e in enumerate(conn.entries):
-#             # if index >= 4:
-#             #     break  # Sale del bucle después de imprimir las primeras 3 entradas    
-#         ou_name = e.ou.value
-#         print(f"- {ou_name}")
+
+def test():
+    filter = '(objectClass=organizationalUnit)'
+    conn.search('dc=estudiantes,dc=ie,dc=tec,dc=ac,dc=cr', filter, attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+
+    for index, e in enumerate(conn.entries):
+            # if index >= 4:
+            #     break  # Sale del bucle después de imprimir las primeras 3 entradas    
+        ou_name = e.ou.value
+        print(f"- {ou_name}")
         
-# test()
-
-#     # print(f"Entry: {e.dn}")
-    # for attribute in e:
-    #     print(f"  {attribute.key}: {attribute.values}")
-    # print("=" * 50)
-    # print(format_string.format(str(e.name), str(e.displayName), str(e.userPrincipalName)[:19], str(e.accountExpires.value)[:19]))
+test()

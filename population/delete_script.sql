73c8d60b2fa443a6dc969f7c378eb9d5d924a8f8
Shakime Sparks <jeykimers.johnson@gmail.com>
2024-03-20 12:33:03 -0600

    Update delete_script.sql

--- population/delete_script.sql
+++ population/delete_script.sql
@@ -2,13 +2,13 @@
 SET SQL_SAFE_UPDATES = 0;
 
 -- Borrar todas las filas de la tabla
-DELETE FROM sibe_db.equipo;
-DELETE FROM sibe_db.componente;
-DELETE FROM sibe_db.categoria;
+DELETE FROM sibe_db.Equipo;
+DELETE FROM sibe_db.Componente;
+DELETE FROM sibe_db.Categoria;
 -- Habilitar claves de seguridad después de borrar las filas
 SET SQL_SAFE_UPDATES = 1;
 
 -- Reiniciar el índice autoincremental a 1
-ALTER TABLE sibe_db.equipo AUTO_INCREMENT = 1;
-ALTER TABLE sibe_db.componente AUTO_INCREMENT = 1;
-ALTER TABLE sibe_db.categoria AUTO_INCREMENT = 1;
+ALTER TABLE sibe_db.Equipo AUTO_INCREMENT = 1;
+ALTER TABLE sibe_db.Componente AUTO_INCREMENT = 1;
+ALTER TABLE sibe_db.Categoria AUTO_INCREMENT = 1;
